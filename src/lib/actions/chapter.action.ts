9f6c50f903f05018c310c6e7e8ad9af9b4046f95
Vince Alwyn Sibay <sibay.vincealwyn@gmail.com>
2024-12-28 11:48:25 +0800

    Improve error handling

--- src/lib/actions/chapter.action.ts
+++ src/lib/actions/chapter.action.ts
@@ -4,62 +4,68 @@ import prisma from "../db";
 import { checkUser } from "./common";
 
 export async function createChapter(novelId: number) {
-  const isValidUser = await checkUser();
-  if (!isValidUser.success) {
-    return isValidUser;
-  }
+  try {
+    const isValidUser = await checkUser();
+    if (!isValidUser.success) {
+      return isValidUser;
+    }
 
-  const novel = await prisma.novel.findFirst({
-    where: {
-      id: novelId,
-    },
-    include: {
-      _count: {
-        select: {
-          chapter: true,
+    const novel = await prisma.novel.findFirst({
+      where: {
+        id: novelId,
+      },
+      include: {
+        _count: {
+          select: {
+            chapter: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  const chapter = await prisma.chapter.create({
-    data: {
-      title: `Untitled Chapter ${novel?._count.chapter}`,
-      content: "",
-      novelId: novelId,
-      published: false,
-    },
-  });
+    if (!novel) {
+      return { message: "Novel not found.", success: false };
+    }
 
-  console.log(JSON.stringify(chapter));
+    const chapter = await prisma.chapter.create({
+      data: {
+        title: `Untitled Chapter ${novel?._count.chapter}`,
+        content: "",
+        novelId: novelId,
+        published: false,
+      },
+    });
 
-  return {
-    message: "Novel Chapter successfully created",
-    data: chapter,
-    success: true,
-  };
-
-  // if (!chapter) {
-  //   return {message: "Something went wrong", success: false}
-  // }
+    return {
+      message: "Novel Chapter successfully created",
+      data: chapter,
+      success: true,
+    };
+  } catch (e) {
+    return { message: e.message, success: false };
+  }
 }
 
 export async function getNovelChapter(chapterId: number) {
-  const isValidUser = await checkUser();
-  if (!isValidUser.success) {
-    return isValidUser;
-  }
+  try {
+    const isValidUser = await checkUser();
+    if (!isValidUser.success) {
+      return isValidUser;
+    }
 
-  const chapter = await prisma.chapter.findFirst({
-    where: {
-      id: chapterId,
-    },
-  })
+    const chapter = await prisma.chapter.findFirst({
+      where: {
+        id: chapterId,
+      },
+    });
 
-  console.log(JSON.stringify(chapter))
+    console.log(JSON.stringify(chapter));
 
-  return {
-    data: chapter,
-    success:true
+    return {
+      data: chapter,
+      success: true,
+    };
+  } catch (e) {
+    return { message: e.message, success: false };
   }
 }
--- src/lib/actions/novel.action.ts
+++ src/lib/actions/novel.action.ts
@@ -11,44 +11,49 @@ export async function createNovel(
   prevState: FormState,
   formData: FormData
 ): Promise<FormState> {
-  const isValidUser = await checkUser();
-  if (!isValidUser.success) {
-    return isValidUser;
-  }
-
-  const validatedFields = novelSchema.safeParse({
-    title: formData.get("title"),
-    synopsis: formData.get("synopsis"),
-    author: formData.get("author"),
-    releaseYear: formData.get("releaseYear"),
-  });
-
-  if (!validatedFields.success) {
-    console.log(validatedFields.error.issues.map((err) => err.message + "\n"));
-    return {
-      message: "Please fix errors in form",
-      fields: validatedFields.data,
-      success: false,
-    };
-  }
-
   try {
+    const isValidUser = await checkUser();
+    if (!isValidUser.success) {
+      return isValidUser;
+    }
+
+    const validatedFields = novelSchema.safeParse({
+      title: formData.get("title"),
+      synopsis: formData.get("synopsis"),
+      author: formData.get("author"),
+      releaseYear: formData.get("releaseYear"),
+    });
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.issues.map((err) => err.message + "\n")
+      );
+      return {
+        message: "Please fix errors in form",
+        fields: validatedFields.data,
+        success: false,
+      };
+    }
+
     await prisma.novel.create({
       data: {
         title: validatedFields.data.title,
         synopsis: validatedFields.data.synopsis,
         author: validatedFields.data.author,
         releaseYear: validatedFields.data.releaseYear,
-        slug: generateSlug(validatedFields.data.title)
+        slug: generateSlug(validatedFields.data.title),
       },
     });
     return { message: "Novel created successfully.", success: true };
   } catch (e) {
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       // The .code property can be accessed in a type-safe manner
-      console.log(e.code)
+      console.log(e.code);
       if (e.code === "P2002") {
-        return { message: "This novel title has already been taken.", success: false };
+        return {
+          message: "This novel title has already been taken.",
+          success: false,
+        };
       }
     }
     return { message: e.message, success: false };
@@ -56,31 +61,39 @@ export async function createNovel(
 }
 
 export async function getNovels() {
-  const isValidUser = await checkUser();
-  if (!isValidUser.success) {
-    return isValidUser;
+  try {
+    const isValidUser = await checkUser();
+    if (!isValidUser.success) {
+      return isValidUser;
+    }
+
+    const novels = (await prisma.novel.findMany()) ?? [];
+
+    return {
+      data: novels,
+    };
+  } catch (e) {
+    return { message: e.message, success: false };
   }
-
-  const novels = await prisma.novel.findMany() ?? [];
-
-  return {
-    data: novels,
-  };
 }
 
 export async function getNovel(slug: string) {
-  const isValidUser = await checkUser();
-  if (!isValidUser.success) {
-    return isValidUser;
+  try {
+    const isValidUser = await checkUser();
+    if (!isValidUser.success) {
+      return isValidUser;
+    }
+
+    const novel = await prisma.novel.findFirst({
+      where: {
+        slug: slug,
+      },
+    });
+
+    return {
+      data: novel,
+    };
+  } catch (e) {
+    return { message: e.message, success: false };
   }
-
-  const novel = await prisma.novel.findFirst({
-    where: {
-      slug: slug,
-    },
-  });
-
-  return {
-    data: novel
-  };
 }
