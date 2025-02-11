9cc04ffebd696de2d745978c2dc97aeef004bee5
Vince Alwyn Sibay <sibay.vincealwyn@gmail.com>
2025-02-11 20:20:54 +0800

    Implement schedule publish chapter

--- src/components/PublishButton.tsx
+++ src/components/PublishButton.tsx
@@ -3,6 +3,19 @@
 import { publishChapter } from "@/lib/actions/chapter.action";
 import { Button } from "./ui/button";
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { DateTimePicker } from "./ui/datetime-picker";
+import { addMinutes } from "date-fns";
+
 export default function PublishButton({
   chapterId,
   isSaving,
@@ -10,13 +23,42 @@ export default function PublishButton({
   chapterId: number;
   isSaving: boolean;
 }) {
+  const [date, setDate] = useState<Date | undefined>(
+    new Date(addMinutes(new Date(), 5))
+  );
+  const handleSchedulePublishChapter = async () => {
+    await publishChapter(chapterId, date as Date);
+  };
   const handlePublishChapter = async () => {
-    await publishChapter(chapterId);
+    await publishChapter(chapterId, new Date());
   };
 
   return (
-    <Button onClick={() => handlePublishChapter()} disabled={isSaving}>
-      Publish
-    </Button>
+    <Dialog>
+      <DialogTrigger>Publish</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Set Publish Schedule</DialogTitle>
+        </DialogHeader>
+        <DateTimePicker hourCycle={12} value={date} onChange={setDate} />
+
+        <DialogFooter className="justify-end mt-4 flex flex-row items-center gap-4">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            onClick={() => handleSchedulePublishChapter()}
+            disabled={isSaving}
+          >
+            Schedule Publish
+          </Button>
+          <Button onClick={() => handlePublishChapter()} disabled={isSaving}>
+            Publish Now
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
